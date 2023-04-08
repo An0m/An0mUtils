670ba60729b64b6d8241654b80bd590e8c1ecf1b
An0m <admin@an0m.dev>
2023-04-08 17:45:58 +0200

    Removed first / from Spigot's getPlayerIP

--- src/main/java/dev/an0m/mcutils/spigot/PlayerUtils.java
+++ src/main/java/dev/an0m/mcutils/spigot/PlayerUtils.java
@@ -136,6 +136,6 @@ public class PlayerUtils {
 
     /** Returns a player's IP address (IPv4 or IPv6) */
     public static String getPlayerIP(Player player) {
-        return player.getAddress().getAddress() + "";
+        return (player.getAddress().getAddress() + "").substring(1); // Removes the first slash (/)
     }
 }
